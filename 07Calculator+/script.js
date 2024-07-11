ec62a7f1cf401ea2cb101b475315d62d38313914
유경 <eeheueklf@gmail.com>
2024-07-11 12:58:01 +0900

    Add : 07Calculator+

--- 07Calculator+/script.js
+++ 07Calculator+/script.js
@@ -1,41 +1,52 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('button');
     const window = document.getElementById('window');
+    let reset = false;
+    let oper = false;
 
     //window에 클릭한 값을 보여준다
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             let value = button.textContent;
-            let preValue
+            let currentText = window.textContent;
 
-            if (value === 'AC') {
-                window.textContent = '0';
-            } else if (value === 'del') {
-                let currentText = window.textContent;
-                if (currentText.length > 1) {
-                    window.textContent = currentText.slice(0, -1);
-                } else {
-                    window.textContent = '0';
-                }
-            } else if (value === '=') {
+            if (value === '=') { // 결과값 출력
+                reset = true;
                 try {
                     let result = eval(window.textContent);
                     window.textContent = result;
                 } catch (error) {
                     window.textContent = 'Error';
                 }
-            } else {
-                if (window.textContent === '0' || window.textContent === 'Error') {
-                    window.textContent = value;
-                } else {
-                    if(preValue|| preValue === '='){
-                        window.textContent = value;
+            }
+            else{
+                // AC 초기화
+                if (value === 'AC') {
+                    window.textContent = '0';
+                } else if (value === 'del') { // del 지우기
+                    if (currentText.length > 1) {
+                        window.textContent = currentText.slice(0, -1);
+                    } else {
+                        window.textContent = '0';
+                    }
+                } else if(value === '+' || value === '-' || value === '*' || value === '/' || value === '%') {
+                    // oper 두번 연속 입력되면 마지막 oper만 표시
+                    if(oper){
+                        window.textContent = currentText.slice(0, -1);
                     }
                     window.textContent += value;
+                    oper = true;
                 }
+                else {
+                    oper=false;
+                    if (reset || window.textContent === '0' || window.textContent === 'Error') {
+                        window.textContent = value;
+                    } else {
+                        window.textContent += value;
+                    }
+                }
+                reset = false;
             }
-            preValue = value;
-
         });
     });
 });
